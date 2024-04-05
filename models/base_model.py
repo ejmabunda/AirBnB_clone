369134a8d79e080b1650c1223be26467acf04836
Junior Mabunda <mjmabunda0@gmail.com>
2024-04-05 10:59:31 +0200

    Add support for variable-length arguments
    - Enhance `__init__` method to handle `args, **kwargs` arguments.
    - Ignore `args`.
    - If `kwargs` is not empty, add each key-value pair as an attribute, excluding `__class__`.
    - Convert `created_at` and `updated_at` from strings to `datetime` objects.
    - If `kwargs` is empty, create `id` and `created_at` for new instances.

--- models/base_model.py
+++ models/base_model.py
@@ -13,10 +13,19 @@ class BaseModel:
         updated_at (datetime): timestamp of when the object was updated.
 
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key is not '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        dt = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        setattr(self, key, dt)
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns the string representation of a BaseModel instance.
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module suppiles the test class for the base model."""
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
@@ -12,13 +13,23 @@ class TestBaseModel(unittest.TestCase):
         self.my_model.name = "My First Model"
         self.my_model.my_number = 89
 
+        my_model_json = self.my_model.to_dict()
+        self.my_new_model = BaseModel(**my_model_json)
+
     def tearDown(self):
         """Called after each test."""
+        pass
+
+    def test__str__(self):
         pass
 
     def test_attributes(self):
         self.assertEqual(self.my_model.name, "My First Model")
         self.assertEqual(self.my_model.my_number, 89)
+        self.assertEqual(type(self.my_model.created_at), datetime)
+
+        self.assertEqual(self.my_new_model.id, self.my_model.id)
+        self.assertEqual(type(self.my_new_model.created_at), datetime)
 
     def test_save(self):
         old_updated_at = self.my_model.updated_at
@@ -33,3 +44,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(model_json["id"], self.my_model.id)
         self.assertTrue(isinstance(model_json["created_at"], str))
         self.assertTrue(isinstance(model_json["updated_at"], str))
+
+    def test_compare_models(self):
+        self.assertIsNot(self.my_model, self.my_new_model)
