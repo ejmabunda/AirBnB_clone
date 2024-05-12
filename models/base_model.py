be8d0211a2dc14c5b65f98ad5deb9143f5b08cf4
Junior Mabunda <mjmabunda0@gmail.com>
2024-05-12 03:32:31 +0200

    Add base model, tests, and gitignore

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module supplies the base class.
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """"Represents the base model.
+
+    Attributes:
+        id (str): Unique identifier of a model instance.
+        created_at (datetime): Date instance was created.
+        updated_at (datetime): Date instance was updated.
+
+    """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """Updates 'updated_at' with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of '__dict__'
+        """
+        my_dict = self.__dict__
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = datetime.isoformat(self.created_at)
+        my_dict['updated_at'] = datetime.isoformat(self.updated_at)
+        return my_dict
---
+++ tests/test_base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module the test class for the base model.
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test cases for the base model.
+    """
+    def setUp(self):
+        """Called before each test.
+        """
+        self.model = BaseModel()
+        self.model.name = "My First Model"
+        self.model.my_number = 89
+
+    def test_init(self):
+        """Test if attributes are initialized correctly.
+        """
+        self.assertEqual(self.model.name, "My First Model")
+        self.assertEqual(self.model.my_number, 89)
+
+    def test_save(self):
+        """Test if save() updates 'updated_at'.
+        """
+        self.model.save()
+        self.assertNotEqual(self.model.created_at, self.model.updated_at)
+
+    def test_to_dict(self):
+        """Check if 'to_dict()' returns dictionary correctly.
+        """
+        model_dict = self.model.to_dict()
+        self.assertEqual(model_dict["name"], "My First Model")
+        self.assertEqual(model_dict["my_number"], 89)
+        self.assertIsInstance(model_dict["id"], str)
+        self.assertIsInstance(model_dict["created_at"], str)
+        self.assertIsInstance(model_dict["updated_at"], str)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+
+
+if __name__ == '__main__':
+    unittest.main()
