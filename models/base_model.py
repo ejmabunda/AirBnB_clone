135aad239ea4ab2ba52dee9efa5186d0aa669fa1
Junior Mabunda <mjmabunda0@gmail.com>
2024-05-16 13:15:13 +0200

    Add support for multivariable arguments, tests

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,16 @@ class BaseModel:
         updated_at (datetime): Date instance was updated.
 
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -30,7 +36,7 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary containing all keys/values of '__dict__'
         """
-        my_dict = self.__dict__
+        my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = datetime.isoformat(self.created_at)
         my_dict['updated_at'] = datetime.isoformat(self.updated_at)
