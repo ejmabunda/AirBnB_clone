bf3e56d9f698c8d99d8c57b37004262b4aed882e
Junior Mabunda <mjmabunda0@gmail.com>
2024-05-17 09:22:35 +0200

    Add documentation, fix prompt

--- console.py
+++ console.py
@@ -1,20 +1,56 @@
 #!/usr/bin/python3
-"""This module supplies the command interpreter.
 """
+This module supplies the command interpreter for the HBNB project.
+It includes a console that uses the cmd module.
+"""
+
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbhb) '
+    """
+    HBNBCommand class:
+    This class is the command interpreter for the HBNB project.
+    It includes methods to handle commands such as quit,
+    EOF, and emptyline.
+    """
+
+    prompt = '(hbnb) '
 
     def do_quit(self, line):
-        """Quit command to exit the program."""
+        """
+        Quit command to exit the program.
+
+        Args:
+            line (str): Not used, but required.
+
+        Returns:
+            True to exit the program.
+        """
         return True
 
     def do_EOF(self, line):
-        """EOF command to exit the program."""
+        """
+        EOF (End Of File) command to exit the program.
+
+        Args:
+            line (str): Not used, but required.
+
+        Returns:
+            True to exit the program.
+        """
         print()
         return True
+
+    def do_emptyline(self, line):
+        """
+        Empty line command. If the user inputs an empty line,
+        nothing should be executed and the prompt should be
+        displayed again.
+
+        Args:
+            line (str): Not used, but required.
+        """
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
