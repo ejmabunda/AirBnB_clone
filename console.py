fbdb108ba2a60acb03b90a08f61773a263f0120b
Junior Mabunda <mjmabunda0@gmail.com>
2024-05-29 10:23:30 +0200

    Implement command intepreter with basic commands
    Command include:
    - quit and EOF to exit the program
    - An empty line + ENTER don't execute anything

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines the HBNBCommand class.
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Custom command interpreter for the AirBnB project.
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program.
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF (Ctrl-D).
+        """
+        print()  # Print a newline before exiting
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered.
+        """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
